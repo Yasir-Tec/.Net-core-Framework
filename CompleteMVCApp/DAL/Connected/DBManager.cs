aff09ae65e767f5401fe9d3a60d6b5964ac396a8
Yasir Shaikh <68939975+Yasir-Tec@users.noreply.github.com>
2023-12-28 05:07:13 -0800

    Add files via upload

---
+++ CompleteMVCApp/DAL/Connected/DBManager.cs
@@ -0,0 +1,57 @@
+namespace DAL.Connected;
+
+using BOL;
+using MySql.Data.MySqlClient;
+
+
+public class DBManager
+{
+    public static List<Product> getAllProducts()
+    {
+
+
+         List<Product> plist = new List<Product>();
+         MySqlConnection conn = new MySqlConnection();
+         conn.ConnectionString="server=192.168.10.150;port=3306;user=dac58;password=welcome;database=dac58";
+         string query = "select * from productsdata";
+
+         try
+         {
+            MySqlCommand command = new MySqlCommand(query,conn);
+            conn.Open();
+
+            MySqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                int id1=int.Parse(reader["id"].ToString());
+                Console.WriteLine(id1);
+                string title1=reader["title"].ToString();
+                string iurl1=reader["imageurl"].ToString();
+                string cat1=reader["category"].ToString();
+                int price1=int.Parse(reader["price"].ToString());
+                Product p=new Product{
+                        id=id1,
+                        title=title1,
+                        imageURL=iurl1,
+                        category=cat1,
+                        Price=price1
+                };
+               System.Diagnostics.Debug.WriteLine("This will be displayed in output window");
+                plist.Add(p);
+              
+
+            }
+         }
+         catch(Exception e)
+         {
+            Console.WriteLine(e.Message);
+         }
+         finally
+         {
+            conn.Close();
+         }
+
+        return plist;
+
+    }
+}
---
+++ CompleteMVCApp/DAL/Connected/EmpManager.cs
@@ -0,0 +1,140 @@
+namespace DAL.Connected;
+
+using BOL;
+using MySql.Data.MySqlClient;
+
+
+public class EmpManager
+{
+   
+
+    public static string LoginEmployee(string username,string password)
+    {
+         Console.WriteLine(username+"  in DAL  "+password);
+         MySqlConnection conn = new MySqlConnection();
+         conn.ConnectionString="server=192.168.10.150;port=3306;user=dac58;password=welcome;database=dac58";
+         string query = "select * from employeedata where username=@username and password=@password";
+        int flag = 0;
+        string name1="";
+         try
+        {
+            MySqlCommand command = new MySqlCommand(query,conn);
+            command.Parameters.AddWithValue("@username",username);
+            command.Parameters.AddWithValue("@password",password);
+
+            conn.Open();
+            
+            MySqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                string username1=reader["username"].ToString();
+                string password1=reader["password"].ToString();
+                if(username==username1 && password==password1)
+                {
+                    name1=reader["name"].ToString();
+                    flag=1;
+                   
+                }
+            }
+            if(flag==1)
+            {
+                Console.WriteLine("Login Successfull");
+                   Console.WriteLine("inside DAL"+name1);
+                return name1;
+            }
+            else
+            {
+                 Console.WriteLine("Login Failed");
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            conn.Close();
+        }
+        return null;
+    }
+
+   public static void RegisterEmployee(int id,string name,string email,string username,string password)
+   {
+         Console.WriteLine("inside DAL");
+         Console.WriteLine(id+" "+name+" "+" "+email+" "+username+" "+password);
+         MySqlConnection conn = new MySqlConnection();
+         conn.ConnectionString="server=192.168.10.150;port=3306;user=dac58;password=welcome;database=dac58";
+         string query = "insert into employeedata values(@id,@name,@email,@username,@password)";
+         try
+        {
+            MySqlCommand command = new MySqlCommand(query,conn);
+            command.Parameters.AddWithValue("@id",id);
+            command.Parameters.AddWithValue("@name",name);
+            command.Parameters.AddWithValue("@email",email);
+            command.Parameters.AddWithValue("@username",username);
+            command.Parameters.AddWithValue("@password",password);
+
+            conn.Open();
+            command.ExecuteNonQuery();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            conn.Close();
+        }
+
+
+    }
+
+ 
+    public static List<Employee> getAllEmployee()
+    {
+
+         
+         List<Employee> plist = new List<Employee>();
+         MySqlConnection conn = new MySqlConnection();
+         conn.ConnectionString="server=192.168.10.150;port=3306;user=dac58;password=welcome;database=dac58";
+         string query = "select * from employeedata";
+
+         try
+         {
+            MySqlCommand command = new MySqlCommand(query,conn);
+            conn.Open();
+
+            MySqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                int id1=int.Parse(reader["id"].ToString());
+                string name1=reader["name"].ToString();
+                string email1=reader["email"].ToString();
+                string username1=reader["username"].ToString();
+                string password1=reader["password"].ToString();
+                Employee emp=new Employee{
+                        id=id1,
+                        name=name1,
+                        email=email1,
+                        username=username1,
+                        password=password1
+                };
+              
+                plist.Add(emp);
+              
+
+            }
+         }
+         catch(Exception e)
+         {
+            Console.WriteLine(e.Message);
+         }
+         finally
+         {
+            conn.Close();
+         }
+
+        return plist;
+
+    }
+}
