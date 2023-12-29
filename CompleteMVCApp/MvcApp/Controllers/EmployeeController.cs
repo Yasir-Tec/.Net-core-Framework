263b3c182cc0e759fae0b109968ca0da2379badd
Yasir Shaikh <68939975+Yasir-Tec@users.noreply.github.com>
2023-12-29 05:46:09 -0800

    Add files via upload

--- CompleteMVCApp/MvcApp/Controllers/EmployeeController.cs
+++ CompleteMVCApp/MvcApp/Controllers/EmployeeController.cs
@@ -5,6 +5,8 @@ using productApp.Models;
 using BOL;
 
 using BLL;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+
 
 
 
@@ -27,6 +29,41 @@ public class EmployeeController : Controller
         ViewData["employees"]=emplist;
          return View();
     }
+
+    public IActionResult Details(int id)
+    {
+        Console.WriteLine("hii id"+id);
+        return View();
+    }
+
+    public IActionResult Delete(int id)
+    {
+        Console.WriteLine("hii delete"+id);
+        EmployeeManager empManager = new EmployeeManager();
+        empManager.DeleteEmployee(id);
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        EmployeeManager empManager = new EmployeeManager();
+        List<Employee> emplist = empManager.getEmployeeById(id);
+        ViewData["employees"]=emplist;
+        return View();
+    }
+    
+
+    [HttpPost]
+    public IActionResult Edit(int id,string name,string email,string username,string password)
+    {
+
+        EmployeeManager empManager = new EmployeeManager();
+        empManager.EditEmployee(id,name,email,username,password);
+        Console.WriteLine(id+" "+name+" "+" "+email+" "+username+" "+password);
+        return View();
+    }
+  
 
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
